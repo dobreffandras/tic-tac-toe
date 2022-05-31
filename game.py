790a7a8ea6a31c09d648947b7a93f7a41ed55064
András Dobreff <dobreffandras@gmail.com>
2022-05-31 15:36:37 +0200

    Split creation and layout of button controls

--- game.py
+++ game.py
@@ -3,15 +3,22 @@ from tkinter import Tk, Button
 class Game(Tk):
     def __init__(self):
         super().__init__()
+        self.playfield_buttons = {}
+        self.indexes = [(r, c) for r in range(3) for c in range(3)]
         self.title("Tic tac toe")
         self.config(width=800, height=600)
-        self.layout()
+        self.setup_controls()
+        self.layout_controls()
 
-    def layout(self):
-        indexes = [(r, c) for r in range(3) for c in range(3)]
-        for r, c in indexes:
+    def setup_controls(self):
+        for r, c in self.indexes:
             text = f"Button ({r},{c})"
             btn = Button(self, text=text)
+            self.playfield_buttons[(r,c)] = btn
+    
+    def layout_controls(self):
+        for key, btn in self.playfield_buttons.items():
+            r, c = key
             btn.grid(row=r, column=c)
             
     def launch(self):
