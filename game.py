f74868b1c08009b50b32891379ff2fa6cea04001
András Dobreff <dobreffandras@gmail.com>
2022-05-31 15:29:26 +0200

    Add buttons for the 3x3 playfield

--- game.py
+++ game.py
@@ -1,9 +1,18 @@
-from tkinter import Tk
+from tkinter import Tk, Button
 
 class Game(Tk):
     def __init__(self):
         super().__init__()
         self.title("Tic tac toe")
         self.config(width=800, height=600)
+        self.layout()
+
+    def layout(self):
+        indexes = [(r, c) for r in range(3) for c in range(3)]
+        for r, c in indexes:
+            text = f"Button ({r},{c})"
+            btn = Button(self, text=text)
+            btn.grid(row=r, column=c)
+            
     def launch(self):
         self.mainloop()
