aabb0fff8c60b55ac9c652eb3740baa4e9f22c48
András Dobreff <dobreffandras@gmail.com>
2022-06-01 12:46:07 +0200

    Let the computer simulate thinking

--- computer_player.py
+++ computer_player.py
@@ -1,6 +1,8 @@
 from game_play_state import GamePlayState
+from time import sleep
 
 
 class ComputerPlayer:
     def next_move(self, board: GamePlayState.GameBoard) -> tuple[int, int]:
+        sleep(1/3)
         return next((move for (move, val) in board.items() if val is None))
