2b21d52d566ab507e7b83ca4e4af17f959a75bd5
András Dobreff <dobreffandras@gmail.com>
2022-06-08 09:45:32 +0200

    Add strategy class and builder scaffold

--- strategy.py
+++ strategy.py
@@ -1,4 +1,6 @@
 import abc
+from pathlib import Path
+
 from game_play_state import GamePlayState
 
 
@@ -17,7 +19,22 @@ class BasicStrategyBuilder:
     def build(self) -> Strategy:
         return BasicStrategy()
 
+class ComputerStrategy(Strategy):
+    def step(self, board: GamePlayState.GameBoard) -> tuple[int,int]:
+        return (0,0) # TODO write logic for stepping
 
 class ComputerStrategyBuilder:
+    def __init__(self, computer_strategy_file_path: str):
+        self.file = Path(computer_strategy_file_path)
+
     def build(self) -> Strategy:
+        if self.file.exists():
+            # TODO load strategy from file
+            ...
+        else:
+            strategy = self.build_strategy()
+            # TODO save strategy to file
+            return strategy
+
+    def build_strategy(self) -> Strategy:
         pass
